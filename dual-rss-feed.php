1714c39538b6acaea9bbf0db01089ca5f556179a
jeremyfelt <jeremy.felt@gmail.com>
2014-09-09 15:31:44 -0700

    Capture the secret key used for the full text RSS feed

--- dual-rss-feed.php
+++ dual-rss-feed.php
@@ -13,6 +13,7 @@ class WSU_Dual_RSS_Feed {
 	 */
 	public function __construct() {
 		add_filter( 'pre_option_rss_use_excerpt', array( $this, 'override_excerpt' ) );
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
 	}
 
 	/**
@@ -25,14 +26,61 @@ class WSU_Dual_RSS_Feed {
 		if ( is_feed() && isset( $_GET['dual_rss_feed_key'] ) ) {
 			$dual_rss_feed_key = sanitize_key( $_GET['dual_rss_feed_key'] );
 
-			$expected_key = get_option( 'dual_rss_feed_key', false );
+			$expected_key = get_option( 'wsu_drf_options', false );
 
-			if ( $dual_rss_feed_key === $expected_key ) {
+			if ( isset( $expected_key['secret_key'] ) && $dual_rss_feed_key === $expected_key['secret_key'] ) {
 				return 0;
 			}
 		}
 
 		return false;
+	}
+
+	/**
+	 * Register the setting used to hold the secret key.
+	 */
+	public function admin_init() {
+		register_setting( 'reading', 'wsu_drf_options', array( $this, 'sanitize_key' ) );
+		add_settings_field( 'wsu_drf_key', 'Dual RSS Secret Key', array( $this, 'secret_key_input' ), 'reading', 'default' );
+	}
+
+	/**
+	 * Sanitize the value input for the Dual RSS Feed Secret Key.
+	 *
+	 * @param $input Array of inputs. In this case, we expect one key.
+	 *
+	 * @return array Inputs with modifications.
+	 */
+	public function sanitize_key( $input ) {
+		if ( isset( $input['secret_key'] ) ) {
+			$sanitized_input = array();
+			$sanitized_input['secret_key'] = sanitize_key( $input['secret_key'] );
+		} else {
+			return array();
+		}
+
+		return $sanitized_input;
+	}
+
+	/**
+	 * Provide a space to input the secret key on the Reading Options page.
+	 */
+	public function secret_key_input() {
+		$wsu_drf_options = get_option( 'wsu_drf_options', false );
+
+		if ( isset( $wsu_drf_options['secret_key'] ) ) {
+			$dual_rss_feed_key = $wsu_drf_options['secret_key'];
+		} else {
+			$dual_rss_feed_key = '';
+		}
+
+		?>
+		<input name="wsu_drf_options[secret_key]" id="wsu_drf_key" value="<?php echo esc_attr( $dual_rss_feed_key ); ?>" />
+		<?php if ( '' === $dual_rss_feed_key ) : ?>
+			<p class="description">Input a secret key here to attach to your RSS feed for a full text version. (e.g. <?php echo esc_url( home_url( '/feed/?dual_rss_feed_key=' ) ); ?>)</p>
+		<?php else : ?>
+			<p class="description">The full text RSS feed URL for your site is <?php echo esc_url( home_url( '/feed/?dual_rss_feed_key=' . $dual_rss_feed_key ) ); ?></p>
+		<?php endif;
 	}
 }
 new WSU_Dual_RSS_Feed();
