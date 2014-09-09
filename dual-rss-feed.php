c431444d67c6092e1f75595508c3f22f05242756
jeremyfelt <jeremy.felt@gmail.com>
2014-09-09 14:32:03 -0700

    Check a passed secret key to determine RSS full text access

--- dual-rss-feed.php
+++ dual-rss-feed.php
@@ -8,8 +8,31 @@ Version: 0.0.1
 */
 
 class WSU_Dual_RSS_Feed {
+	/**
+	 * Set things up.
+	 */
 	public function __construct() {
+		add_filter( 'pre_option_rss_use_excerpt', array( $this, 'override_excerpt' ) );
+	}
 
+	/**
+	 * Determine if we are overriding the default RSS setting based on the existence of
+	 * a secret key and provide the option value if so.
+	 *
+	 * @return int|bool 0 if we are overriding the default setting. False to allow default setting.
+	 */
+	public function override_excerpt() {
+		if ( is_feed() && isset( $_GET['dual_rss_feed_key'] ) ) {
+			$dual_rss_feed_key = sanitize_key( $_GET['dual_rss_feed_key'] );
+
+			$expected_key = get_option( 'dual_rss_feed_key', false );
+
+			if ( $dual_rss_feed_key === $expected_key ) {
+				return 0;
+			}
+		}
+
+		return false;
 	}
 }
 new WSU_Dual_RSS_Feed();
